a31aa2b38d044f41034b3ecec4a484dfc7926bd7
vntero <shin.antero@gmail.com>
2024-10-01 16:27:45 +0200

    improves makejson func to read multiple strings as input

--- packages/makejson.go
+++ packages/makejson.go
@@ -1,35 +1,36 @@
 package packages
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func MakeJson() {
-	// create a map
-	person := make(map[string]string)
+		// create a map
+		person := make(map[string]string)
 
-	// prompts user to enter first name and store it
-	fmt.Print("Enter your first name: ")
-	var name string
-	fmt.Scan(&name)
-
-	// prompts user to enter address and store it
-	fmt.Print("Enter your address: ")
-	var address string
-	fmt.Scan(&address)
-
-	// atrribute name and address to the map
-	person["name"] = name
-	person["address"] = address
-
-	// use the Marshall() to create JSON object from the map
-	jsonData, err := json.Marshal(person)
-	if err != nil {
-		fmt.Println("Unable to marshall JSON: ", err)
-		return
-	}
-
-	// print the JSON object
-	fmt.Println("Here is the JSON representation of the entered data: ", string(jsonData))
+		// prompt user to enter first name and store it
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter your first name: ")
+		name, _ :=  reader.ReadString('\n')
+	
+		// prompt user to enter address and store it
+		fmt.Print("Enter your address: ")
+		address, _ := reader.ReadString('\n')
+	
+		// atrribute name and address to the map
+		person["name"] = name[:len(name)-1]
+		person["address"] = address[:len(address)-1]
+	
+		// use the Marshall() to create JSON object from the map
+		jsonData, err := json.Marshal(person)
+		if err != nil {
+			fmt.Println("Unable to marshall JSON: ", err)
+			return
+		}
+	
+		// print the JSON object
+		fmt.Println("Here is the JSON representation of the entered data: ", string(jsonData))
 }
