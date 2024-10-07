8f16c3d905da86b05757771d2edff5a5982c8b96
vntero <shin.antero@gmail.com>
2024-10-07 08:55:59 +0200

    adds previous solution as comment

--- packages/displacement.go
+++ packages/displacement.go
@@ -35,3 +35,27 @@ func Displacement() {
 	displacement := fn(t)
 	fmt.Println("Displacement after", t, "seconds is", displacement)
 }
+
+// // prompt the user for values
+// fmt.Println("Enter values for acceleration, initial velocity, and initial displacement.")
+
+// // grab what the user typed
+// reader := bufio.NewReader(os.Stdin)
+// input, _ := reader.ReadString('\n')
+
+// // grab each value individually
+// elements := strings.Fields(input)
+
+// // convert values from string to int
+// numbers := make([]int, 0, len(elements))
+// for _, element := range elements {
+// 	num, err := strconv.Atoi(element)
+// 	if err != nil {
+// 		fmt.Println("Invalid input. Please enter valid integers.")
+// 		return
+// 	}
+// 	numbers = append(numbers, num)
+// }
+
+// // prompt user for time
+// fmt.Println("Enter value for time.")
