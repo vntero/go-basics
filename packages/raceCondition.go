02c2178249f4b5b9cdaf7b9fe75991291bc9eddc
vntero <shin.antero@gmail.com>
2024-10-15 14:58:02 +0200

    adds goRoutine with race condition boilerplate

--- packages/raceCondition.go
+++ packages/raceCondition.go
@@ -18,11 +18,15 @@ func increment(wg *sync.WaitGroup) {
 
 func RaceCondition() {
 	var wg sync.WaitGroup
+	firstNames := []string{"Hugo", "Darren", "Marcus", "Cristiano", "Pep", "Jose", "Ryan", "Paul", "Roy", "Gary"}
+	lastNames := []string{"Neville", "Keane", "Scholes", "Giggs", "Mourinho", "Guardiola", "Ronaldo", "Rashford", "Fletcher", "Antero"}
 
+	// start two goroutines
 	wg.Add(2)
 	go increment(&wg)
 	go increment(&wg)
 
+	// wait for both goroutines to finish
 	wg.Wait()
 
 	fmt.Println("Final counter value:", counter)
