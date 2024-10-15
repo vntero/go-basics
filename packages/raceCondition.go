808e09570f7562b1f357f1586d12234cc9e04dba
vntero <shin.antero@gmail.com>
2024-10-15 16:03:22 +0200

    adds comments explaining race condition

--- packages/raceCondition.go
+++ packages/raceCondition.go
@@ -5,21 +5,23 @@ import (
 	"sync"
 )
 
-// shared global variable
-var counter int
+// shared global variables
+var fullNames = []string{}
+var firstNames = []string{"Hugo", "Darren", "Marcus", "Cristiano", "Pep", "Jose", "Ryan", "Paul", "Roy", "Gary"}
+var	lastNames = []string{"Neville", "Keane", "Scholes", "Giggs", "Mourinho", "Guardiola", "Ronaldo", "Rashford", "Fletcher", "Antero"}
 
 // function that manipulayes the global variable
 func increment(wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 0; i < 1000; i++ {
-		counter = counter + 1
+	for i := 0; i < 10; i++ {
+		fullNames = append(fullNames, firstNames[i])
+		fullNames= append(fullNames, lastNames[i])
 	}
 }
 
 func RaceCondition() {
 	var wg sync.WaitGroup
-	firstNames := []string{"Hugo", "Darren", "Marcus", "Cristiano", "Pep", "Jose", "Ryan", "Paul", "Roy", "Gary"}
-	lastNames := []string{"Neville", "Keane", "Scholes", "Giggs", "Mourinho", "Guardiola", "Ronaldo", "Rashford", "Fletcher", "Antero"}
+	
 
 	// start two goroutines
 	wg.Add(2)
@@ -29,6 +31,13 @@ func RaceCondition() {
 	// wait for both goroutines to finish
 	wg.Wait()
 
-	fmt.Println("Final counter value:", counter)
+	// fullNames slice is being shared between 2 goroutines
+	// and both goroutines modify it by appending data to it
+	// since both goroutines are modifying the shared slice at the same time without any locks or synchronization mechanisms
+	// it may lead to unpredictable behaviour
+	// append is not thread-safe, there is a chance that the slice's internal array gets reallocated while both goroutines are writing it
+	// that's why when printing the total of full names (combination of firstNames + lastNames), each time we get a different value
+	fmt.Println("Number of full names (first + last):", len(fullNames) / 2)
+	fmt.Println("Full names generated (combination of firstNames + lastNames):", fullNames)
 }
 
