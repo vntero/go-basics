0ac0bdd49747c31a70efba1e40aa499dd8d44f22
vntero <shin.antero@gmail.com>
2024-10-18 22:25:13 +0200

    replaces for loop with sorts method

--- packages/concurrency.go
+++ packages/concurrency.go
@@ -5,26 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
 )
 
 // main goroutine should merge the 4 sorted subarrays into one large sorted array
-
-// each goroutine which sorts a quarter of the array should print the subarray that it will sort
 // when sorting is complete, the main goroutine should print the entire sorted list
+
 func sorter(a []int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	n := len(a)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if a[j] > a[j+1] {
-				Swap(a, j)
-			}
-		}
-	}
+	// EACH GOROUTINE WHICH SORTS A QUARTER OF THE ARRAY SHOULD PRINT THE SUB-ARRAY THAT IT WILL SORT
+	fmt.Println("Starting to sort the following sub-array:", a)
+
+	sort.Ints(a)
 }
 
 func SortArray() {
@@ -59,6 +55,7 @@ func SortArray() {
 	log.Printf("numbers: %v\n", numbers)
 
 	// PARTITION ARRAY INTO 4 PARTS
+	// EACH PARTITION SHOULD BE APPROX. EQUAL SIZE
 	n := len(numbers) / 4
 	array1 := numbers[0:n]
 	array2 := numbers[n : 2*n]
@@ -66,7 +63,6 @@ func SortArray() {
 	array4 := numbers[3*n:]
 
 	// handle spare parts
-	// EACH PARTITION SHOULD BE APPROX. EQUAL SIZE
 	if len(numbers)%4 != 0 {
 		array4 = append(array4, numbers[4*n:]...)
 	}
@@ -78,9 +74,4 @@ func SortArray() {
 	go sorter(array3, &wg)
 	go sorter(array4, &wg)
 	wg.Wait()
-
-	log.Printf("array1: %v\n", array1)
-	log.Printf("array2: %v\n", array2)
-	log.Printf("array3: %v\n", array3)
-	log.Printf("array4: %v\n", array4)
 }
